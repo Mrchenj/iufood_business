a64ef706aab5c351a82cffb44a6d2d87342c27bc
Mrchenj <860236831@qq.com>
2020-01-23 14:09:58 +0800

    up

--- src/views/login/LoginIndex.vue
+++ src/views/login/LoginIndex.vue
@@ -1,25 +1,90 @@
 <template>
-  <div >
-    <button @click="loginAction" class="login_button"> 登录</button>
+  <div class="login-page">
+
   </div>
 </template>
 
-
 <script>
-export default {
-  components: {
-
-  },
-  methods: {
-    loginAction() {
-      this.$router.push('/');
-    },
-  },
-};
+// import { login } from "@/api/user.js"
+// import {mapActions, mapState, mapGetters} from 'vuex'
+// import store from '@/store/index'
+// export default {
+//   computed: {
+//     canNext() {
+//       return !(Boolean(this.user.loginId.length) && Boolean(this.user.password.length))
+//     },
+//   },
+//   methods: {
+//     ...mapActions([
+//       'setUser'
+//     ]),
+//     onClickLeft() {
+//       this.$router.go(-1)
+//     },
+//     register() {
+//       this.$router.push('register')
+//     },
+//     forget() {
+//       this.$router.push('forget')
+//     },
+//     loginNetwork() {
+//       login(this.user).then(response => {
+//         const { code, data } = response
+//         // setUser(data.userInfo)
+//         if (code === 1 ){
+//           store.commit('setUserInfo', data.userInfo)
+//           store.commit('setUserToken', data.token)
+//           this.$router.push('/member')
+//         }
+//       })
+//     }
+//   },
+//   data() {
+//     return {
+//       value: "",
+//       checked: true,
+//       autosize:{ maxHeight: 211, minHeight: 211 },
+//       user: {
+//         loginId: 'jianpeng',
+//         password: '123abc',
+//         loginType: '0',
+//         userType: ''
+//       }
+//     }
+//   }
+// }
 </script>
 
 <style lang="scss" scoped>
-.login_button {
-  background-color: $iu-oringe;
+.login-page{
+  min-height: 100%;
+  position: relative;
+  font-size: 16px;
+  @include iu-bg;
+}
+
+.other-box {
+  padding: 13px 0px;
+  margin: 0px 13px;
+  display: flex;
+  align-items: flex-end ;
+  justify-content: space-between;
+
+  border-bottom: 1px solid #BABABA
+}
+.third-box {
+  margin: 0px 13px;
+
+  display: flex;
+  align-items: flex-end ;
+  justify-content: center;
+
+  padding: 13px 0px;
+  button:first-child {
+    margin-right: 20px;
+  }
+  .van-button--small {
+    padding: 0px 20px;
+  }
 }
 </style>
