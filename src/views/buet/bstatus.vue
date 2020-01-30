b2a3d14ace17ff5297bd53e8bf0b9ee8d94895c0
Mrchenj <860236831@qq.com>
2020-01-30 21:18:37 +0800

    Update bstatus.vue

--- src/views/buet/bstatus.vue
+++ src/views/buet/bstatus.vue
@@ -1,14 +1,36 @@
 <template>
-  <div class="home">
-
-    營業狀態
-
+  <div class="tent-main">
+    <h3 class="status-title">當前狀態：<b>{{status}}</b></h3>
+    <van-grid :column-num="3">
+      <van-grid-item
+        v-for="value in menuList"
+        :key="value"
+        :icon="value.icon"
+        :text="value.text"
+      />
+    </van-grid>
   </div>
 </template>
 
 <script>
-
-
+export default {
+  data() {
+    return {
+      status: '關閉',
+      menuList: [
+        {
+          text: '營業設置',
+          icon: 'h-sbusi',
+          to: '/business',
+        },
+        {
+          text: '營業菜單',
+          icon: 'h-mbusi',
+        },
+      ],
+    };
+  },
+};
 </script>
 
 
