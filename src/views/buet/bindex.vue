fc1f8ca29b76e256ea55b3ab0928d69fb3e52580
Mrchenj <860236831@qq.com>
2020-01-30 21:18:27 +0800

    關門打烊

--- src/views/buet/bindex.vue
+++ src/views/buet/bindex.vue
@@ -1,37 +1,46 @@
 <template>
   <div class="page">
     <div class="iu-nav">
-      <van-nav-bar title="标题" left-text="返回" left-arrow>
-        <van-icon name="search" slot="right" />
+      <van-nav-bar :title="title" left-arrow @click-left="onClickLeft" @click-right="onClickRight">
+        <div slot="right" >
+          <span>員工編號112</span>
+          <van-button plain type="primary" to='/login'>退出</van-button>
+        </div>
       </van-nav-bar>
     </div>
-    <van-sidebar v-model="activeKey">
-      <van-sidebar-item title="标签名称" />
-      <van-sidebar-item title="标签名称" />
-      <van-sidebar-item title="标签名称" />
-    </van-sidebar>
-    <router-link to="/business/status">開業打烊</router-link>
-    <router-link to="/business/time">營業時間設置</router-link>
-    <router-view></router-view>
-
+    <div class="iu-page-main">
+      <div class="iu-sidebar">
+        <h2>{{title}}</h2>
+        <van-sidebar v-model="activeKey">
+          <van-sidebar-item title="開業打烊" to="/business/status" />
+          <van-sidebar-item title="營業時間設置" to="/business/time" />
+        </van-sidebar>
+      </div>
+      <div class="iu-tent-main">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { Toast } from 'vant';
+
 export default {
   data() {
     return {
       activeKey: 0,
+      title: '營業設置',
     };
   },
-  // methods: {
-  //   onClickLeft() {
-  //     Toast('返回');
-  //   },
-  //   onClickRight() {
-  //     Toast('按钮');
-  //   },
-  // },
+  methods: {
+    onClickLeft() {
+      Toast('返回');
+    },
+    onClickRight() {
+      Toast('退出');
+    },
+  },
 };
 </script>
 
