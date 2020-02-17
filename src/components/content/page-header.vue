e672799e285338e1f5b6b33da00f017d286a02d3
Mrchenj <860236831@qq.com>
2020-02-17 23:29:00 +0800

    組件化

---
+++ src/components/content/page-header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header class="iu-nav">
+      <van-nav-bar :title="title" left-arrow @click-left="onClickLeft">
+        <div slot="left">
+            <van-icon name="wap-home" />
+        </div>
+        <div slot="right" >
+          <span>{{member}}</span>
+          <van-button plain type="primary" to='/login'>退出</van-button>
+        </div>
+      </van-nav-bar>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'pageHeader',
+  data() {
+    return {
+      member: '員工編號113332',
+    };
+  },
+  props: ['title'],
+  methods: {
+    onClickLeft() {
+      this.$router.push('/home/');
+    },
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/content/page-sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+    <aside class="iu-sidebar">
+        <h2 class="iu-siderbar-title">{{title}}</h2>
+        <van-sidebar v-model="activeKey">
+            <van-sidebar-item v-for="item in sidebarMessage" v-bind:key="item.id"
+             :class="item.sideClass" :title="item.sideTitle" :to="item.sideTo"  />
+        </van-sidebar>
+    </aside>
+</template>
+
+<script>
+export default {
+  name: 'pageSidebar',
+  data() {
+    return {
+      activeKey: 0,
+    };
+  },
+  props: ['title', 'sidebarMessage'],
+  methods: {
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+.iu-siderbar-title{
+    text-align: center
+}
+</style>
